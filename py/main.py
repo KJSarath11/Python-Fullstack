b063784fdd3ff51bb475f3ea0abcf5776e7984de
Sarath K J <kjsarath76@gmail.com>
2024-07-15 22:14:23 +0530

    python sql updated

---
+++ py/main.py
@@ -0,0 +1,17 @@
+# s="My name is sarath"
+# a=s.upper()
+# print(a)
+
+# name=s.replace("sarath","kjsarath")
+# print(name)
+
+list=[10,10.5,True,'hey',[1,2]]
+list.append(100)
+print(list)
+list.extend([100,60])
+print(list)
+list.insert(2,'hey')
+print(list)
+
+
+
