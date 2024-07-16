ca4a4146fdcdf59bbeded6ce6fea0826a20af323
Sarath K J <kjsarath76@gmail.com>
2024-07-16 16:02:39 +0530

    Html core attributes

--- py/main.py
+++ py/main.py
@@ -5,13 +5,24 @@
 # name=s.replace("sarath","kjsarath")
 # print(name)
 
-list=[10,10.5,True,'hey',[1,2]]
-list.append(100)
-print(list)
-list.extend([100,60])
-print(list)
-list.insert(2,'hey')
-print(list)
-
-
-
+# list=[10,20,30,40]
+# list.append(100)
+# print(list)
+# list.extend([100,60])
+# print(list)
+# list.insert(2,'hey')
+# print(list)
+# list.pop(1)
+# print(list)
+# list.remove('hey')
+# a=list.count(10)
+# print(a)
+# l1=list.copy()
+# print(l1)
+# list.reverse()
+# print(list)
+# list=[1,4,3,5,2]
+# # list.sort()
+# # print(list)
+# list.sort(reverse=True)
+# print(list)
---
+++ py/tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+s="My name is sarath"
+# a=s.upper()
+# print(a)
+
+# name=s.replace("sarath","kjsarath")
+# print(name)
