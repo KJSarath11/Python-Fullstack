8cb8214079f9af8d8f666961da9379f2fe13b603
Sarath K J <kjsarath76@gmail.com>
2024-07-20 13:23:15 +0530

    Python

---
+++ py/operators.py
@@ -0,0 +1,21 @@
+# # Arithmetic operators
+# a='hello'+'hey'
+# b=1+2
+
+# print(a)
+# print(b)
+
+# Relational operators
+a=12
+b=10
+print(a>b)
+print(a<b)
+print(a<=b)
+print(a>=b)
+print(a==b)
+print(a!=b)
+
+#Logical Operators
+
+
+
